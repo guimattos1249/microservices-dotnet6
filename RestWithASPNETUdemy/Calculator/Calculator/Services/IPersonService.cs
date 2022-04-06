3a40df97dc792dfbce9bbc0162150765e110c738
Guilherme Mattos <guilhermevsmattos@outlook.com>
2022-04-06 15:32:24 -0300

    Creating a simple api

---
+++ RestWithASPNETUdemy/Calculator/Calculator/Services/IPersonService.cs
@@ -0,0 +1,15 @@
+﻿using Calculator.Model;
+
+namespace Calculator.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+
+
+    }
+}
